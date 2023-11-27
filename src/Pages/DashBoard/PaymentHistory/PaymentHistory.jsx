bc1adc8006a52ae820073f32901119cf2a966d98
rishanzihad <138320123+rishanzihad@users.noreply.github.com>
2023-11-27 23:42:40 +0600

    update camp half Done

--- src/Pages/DashBoard/PaymentHistory/PaymentHistory.jsx
+++ src/Pages/DashBoard/PaymentHistory/PaymentHistory.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../../AuthProvider/AuthProvider';
 import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
+
 import useAxiosSecure from '../../../Hooks/useAxiosSecure';
 
 const PaymentHistory = () => {
